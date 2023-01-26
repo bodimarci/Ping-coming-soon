44a9966968a13cf4438067a8f53dfc196d6e2c6a
bodimarci <94900714+bodimarci@users.noreply.github.com>
2023-01-26 09:55:59 +0100

    Update Footer

--- ping-coming-soon/src/components/Footer.jsx
+++ ping-coming-soon/src/components/Footer.jsx
@@ -4,19 +4,21 @@ import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => (
   <footer className="mt-20">
-    <ul className="flex justify-center p-5">
-      <li className="border border-[#C2D3FF]  rounded-full">
-        <FaFacebookF size={24} className="m-2 text-[#4F7DF3]" />
-      </li>
-      <li className="border border-[#C2D3FF]  rounded-full mx-6">
-        <AiOutlineTwitter size={24} className="m-2 text-[#4F7DF3]" />
-      </li>
-      <li className="border border-[#C2D3FF]  rounded-full">
-        <AiOutlineInstagram size={24} className="m-2 text-[#4F7DF3]" />
-      </li>
-    </ul>
+    <div>
+      <ul className="flex justify-center p-5">
+        <li className="border border-[#C2D3FF]  rounded-full">
+          <FaFacebookF size={24} className="m-2 text-[#4F7DF3]" />
+        </li>
+        <li className="border border-[#C2D3FF]  rounded-full mx-6">
+          <AiOutlineTwitter size={24} className="m-2 text-[#4F7DF3]" />
+        </li>
+        <li className="border border-[#C2D3FF]  rounded-full">
+          <AiOutlineInstagram size={24} className="m-2 text-[#4F7DF3]" />
+        </li>
+      </ul>
+    </div>
     <div className="text-center text-[#969696] pb-16">
-      <p>© Copyright Ping. All rights reserved.</p>
+      <h2>© Copyright Ping. All rights reserved.</h2>
     </div>
   </footer>
 );
